a07eac0e687ab4a5db5c3e257d26680eecd4f827
Sophia Nordlie <sophielyn999@gmail.com>
2019-02-25 21:20:31 -0700

    First commit for Assignment 2

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,73 @@
+package cse360assign2;
+
+/**
+ * This program is a basic calculator that performs addition, subtraction,
+ * multiplication, and division on integers passed in by the user. It stores
+ * and displays the current total and the calculation history.
+ * 
+ * @author  Sophia Nordlie class ID: 513
+ * @version 1.0
+ * @since   2019-02-25
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Calculator – This is the constructor for the calculator.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal – This method returns the result of the calculation(s) 
+	 * performed as an integer. 
+	 * @return int This returns the calculated result.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add – This method takes in an integer and adds it to the current total.
+	 * @param value This is an integer passed by the user.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract – This method takes in an integer and subtracts it from the 
+	 * current total.
+	 * @param value This is an integer passed by the user.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply – This method multiplies the current total by the integer passed to it.
+	 * @param value This is an integer passed by the user.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide – This method divides the current total by the integer passed to it.
+	 * @param value This is an integer passed by the user.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory – This method returns the calculation history as a String.
+	 * @return String This is the calculation history.
+	 */
+	public String getHistory () {
+		return "";
+	}
+
+}
